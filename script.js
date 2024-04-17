e0d15d2f4b9e4ef4218f22fe8ffb05f68c20442b
vasenkom <m.vasenko@yandex.ru>
2024-04-17 21:16:08 +0200

    add function to number and operators buttons

---
+++ script.js
@@ -0,0 +1,51 @@
+const display = document.querySelector(".displayScreen");
+const numbers = document.querySelectorAll(".num");
+const proces = document.querySelectorAll(".proces");
+const deleteBtn = document.querySelector(".delete");
+const devide = document.querySelector(".devide");
+const deleteEverythingBtn = document.querySelector(".deleteEverything");
+const multiply = document.querySelector(".multiply");
+const substract = document.querySelector(".substract");
+const equal = document.querySelector(".equal");
+const addBtn = document.querySelector(".add");
+
+let num1 = "";
+let num2 = "";
+operator = "";
+equalUsed = false;
+
+numbers.forEach((number) => {
+    number.addEventListener('click', function () {
+        if (equalUsed === true) {
+            display.textContent = "";
+            num1 = "";
+            equalUsed = false;
+        }
+        
+        display.textContent += number.textContent;
+        if (operator === "") {
+            num1 += number.textContent;
+            console.log(num1);
+        } else {
+            num2 += number.textContent; 
+        }
+    })
+});
+
+proces.forEach((sign) => {
+    sign.addEventListener('click', function() {
+        if (operator === "" && num1 !== "") {
+            display.textContent = '';
+            operator += sign.textContent;
+            console.log(num1)
+        } else if (num1 === "" || num2 === "") {
+            //do nothing
+        } else {
+            operate(num1, num2, operator);
+            display.textContent += sign.value;
+            operator = button.value;
+        }
+    })
+})
+
+
