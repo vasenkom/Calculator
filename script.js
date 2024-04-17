3c0c5844d38c4a396b020f5d52ef4e16242134a6
vasenkom <m.vasenko@yandex.ru>
2024-04-17 21:55:07 +0200

    start equal function

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ const display = document.querySelector(".displayScreen");
 const numbers = document.querySelectorAll(".num");
 const proces = document.querySelectorAll(".proces");
 const deleteBtn = document.querySelector(".delete");
-const devide = document.querySelector(".devide");
 const deleteEverythingBtn = document.querySelector(".deleteEverything");
-const multiply = document.querySelector(".multiply");
-const substract = document.querySelector(".substract");
 const equal = document.querySelector(".equal");
 const addBtn = document.querySelector(".add");
 
 let num1 = "";
 let num2 = "";
-operator = "";
+let operator = "";
 equalUsed = false;
 
 numbers.forEach((number) => {
@@ -37,30 +34,60 @@ proces.forEach((sign) => {
         if (operator === "" && num1 !== "") {
             display.textContent = '';
             operator += sign.textContent;
-            console.log(num1)
+            display.textContent += sign.textContent;
+            console.log(num1); //debugging
+            console.log(num2); //debugging
         } else if (num1 === "" || num2 === "") {
             //do nothing
         } else {
+            display.textContent = '';
             operate(num1, num2, operator);
-            display.textContent += sign.value;
-            operator = button.value;
+            display.textContent += sign.textContent;
+            operator = sign.textContent;
         }
     })
 })
 
-function add(n1, n2) {
-    return n1 + n2;
+function sum(n1, n2) {
+    return parseInt(n1) + parseInt(n2);
 }
 
 function devide(n1, n2) {
-    return n1 / n2;
+    return parseInt(n1) / parseInt(n2);
 }
 
 function multiply(n1, n2) {
-    return n1 * n2;
+    return parseInt(n1) * parseInt(n2);
 }
 
 function substract(n1, n2) {
-    return n1 - n2;
+    return parseInt(n1) - parseInt(n2);
+}
+
+equal.addEventListener('click', function() {
+    if (num1 != '' && num2 != '') {
+        operate(num1, num2, operator);
+    }
+    
+})
+
+function operate(n1, n2, sign) {
+    if (sign == "+") {
+        result = sum(n1, n2);
+        console.log(result)
+    } else if (sign == "-") {
+        result = substract(n1, n2);
+        console.log(result)
+    } else if (sign == "/") {
+        result = devide(n1, n2);
+        console.log(result)
+    } else if (sign == "*") {
+        result = multiply(n1, n2);
+        console.log(result)
+    }
+    num1 = (Math.round(result *10) / 10).toString();
+    operator = "";
+    num2 = "";
+    display.textContent = num1
 }
 
